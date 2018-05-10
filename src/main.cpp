0f215cda07d8f89408c5c9ad35f736c82ef7cde1
Changjie <changjieguan@gmail.com>
2018-05-10 12:37:01 +0200

    Add PID controllers for throtle and steer angle.

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,17 @@ double rad2deg(double x) { return x * 180 / pi(); }
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
-std::string hasData(std::string s) {
+std::string hasData(std::string s)
+{
   auto found_null = s.find("null");
   auto b1 = s.find_first_of("[");
   auto b2 = s.find_last_of("]");
-  if (found_null != std::string::npos) {
+  if (found_null != std::string::npos)
+  {
     return "";
   }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
+  else if (b1 != std::string::npos && b2 != std::string::npos)
+  {
     return s.substr(b1, b2 - b1 + 1);
   }
   return "";
@@ -33,42 +36,75 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
+  PID pid_throtle;
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  // TODO: Initialize the pid variable.
+  pid.Init(0.184, 1.5e-4, 3.09 * 0.55);
+  pid.Init(0.189, 1.5e-4, 1.7005);
+  pid.Kp = 0.193964; 
+  pid.Ki = 0.00015; 
+  pid.Kd = 1.7006; 
+  pid.dp = 0.00357246; 
+  pid.di = 4.78297e-06; 
+  pid.dd= 0.000714493;
+  
+  pid.Type = 0;
+
+  pid_throtle.Init(0.1, 2.5e-4, 1.4);
+  pid_throtle.Type = 1;
+
+  double set_vel = 40.; //40.;
+  h.onMessage([&pid, &pid_throtle, &set_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
     if (length && length > 2 && data[0] == '4' && data[1] == '2')
     {
       auto s = hasData(std::string(data).substr(0, length));
-      if (s != "") {
+      if (s != "")
+      {
         auto j = json::parse(s);
         std::string event = j[0].get<std::string>();
-        if (event == "telemetry") {
+        if (event == "telemetry")
+        {
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<std::string>());
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
+          double throttle;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
           * [-1, 1].
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+
+          pid_throtle.UpdateError(speed - set_vel);
+          throttle = pid_throtle.TotalError();
+
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+          if ((pid.step + 1) % (pid.N * 30) == 0)
+          {
+            set_vel *= 1.05;
+            pid.step = 0;
+            pid.N *= 1/1.05;
+            std::cout << "reference velocity = " << set_vel << std::endl;
+          }
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          //std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = throttle;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
-      } else {
+      }
+      else
+      {
         // Manual driving
         std::string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
